289c6bee2e144273d3075824ce7599f287cda98a
YYinBigBang <yy32438084@gmail.com>
2024-12-02 23:08:54 +0800

    Align code formatting for consistency

--- Contains Duplicate/solutions.py
+++ Contains Duplicate/solutions.py
@@ -13,8 +13,12 @@ class Solution1(object):
         return False
 
 
-class Solution:
+class Solution2(object):
     def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
         nums.sort()
         for i in range(len(nums) - 1):
             if nums[i] == nums[i+1]:
