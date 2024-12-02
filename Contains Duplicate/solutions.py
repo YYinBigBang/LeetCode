e0b157da0abdc5837af0bf60d914d5266eff82b7
YYinBigBang <yy32438084@gmail.com>
2024-12-02 23:02:47 +0800

    Contains Duplicate

---
+++ Contains Duplicate/solutions.py
@@ -0,0 +1,23 @@
+class Solution1(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        check_set = set()
+        for num in nums:
+            if num in check_set:
+                return True
+            else:
+                check_set.add(num)
+        return False
+
+
+class Solution:
+    def containsDuplicate(self, nums):
+        nums.sort()
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i+1]:
+                return True
+        return False
+
