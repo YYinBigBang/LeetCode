7d22cc380eca409a3748adec9d1d86aa406d9173
YYinBigBang <yy32438084@gmail.com>
2024-12-04 00:30:17 +0800

    Add solution

--- Maximum Subarray/solutions.py
+++ Maximum Subarray/solutions.py
@@ -1,4 +1,25 @@
-class Solution(object):
+class Solution1(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        current_sum = nums[0]
+        max_sum = nums[0]
+        for num in nums[1:]:
+
+            if num > current_sum + num:
+                current_sum = num
+            else:
+                current_sum += num
+
+            if current_sum > max_sum:
+                max_sum = current_sum
+
+        return max_sum
+
+
+class Solution2(object):
     def maxSubArray(self, nums):
         """
         :type nums: List[int]
