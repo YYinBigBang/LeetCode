80f899475cab62cb09e6ce1f1bb506cd32264d4c
YYinBigBang <yy32438084@gmail.com>
2024-12-05 23:57:43 +0800

    Valid Parentheses

---
+++ Valid Parentheses/solutions.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for c in s:
+            if c in "([{":
+                stack.append(c)
+            else:
+                if not stack:
+                    return False
+                elif c == ')' and stack[-1] == '(':
+                    stack.pop()
+                elif c == ']' and stack[-1] == '[':
+                    stack.pop()
+                elif c == '}' and stack[-1] == '{':
+                    stack.pop()
+                else:
+                    return False
+        return False if stack else True
+
