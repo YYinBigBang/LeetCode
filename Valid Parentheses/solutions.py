aefc6e2cf70f20b8311eeeaeb0b3a76dee966ff3
YYinBigBang <yy32438084@gmail.com>
2024-12-06 00:09:10 +0800

    Remove redundant nested condition

--- Valid Parentheses/solutions.py
+++ Valid Parentheses/solutions.py
@@ -8,16 +8,15 @@ class Solution(object):
         for c in s:
             if c in "([{":
                 stack.append(c)
+            elif not stack:
+                return False
+            elif c == ')' and stack[-1] == '(':
+                stack.pop()
+            elif c == ']' and stack[-1] == '[':
+                stack.pop()
+            elif c == '}' and stack[-1] == '{':
+                stack.pop()
             else:
-                if not stack:
-                    return False
-                elif c == ')' and stack[-1] == '(':
-                    stack.pop()
-                elif c == ']' and stack[-1] == '[':
-                    stack.pop()
-                elif c == '}' and stack[-1] == '{':
-                    stack.pop()
-                else:
-                    return False
+                return False
         return False if stack else True
 
