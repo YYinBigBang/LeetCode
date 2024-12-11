e02cc4b587f81fb21f7687ab6f503d9d8a4ae467
YYinBigBang <yy32438084@gmail.com>
2024-12-11 14:28:49 +0800

    Add solutions

--- Maximum Depth of Binary Tree/solutions.py
+++ Maximum Depth of Binary Tree/solutions.py
@@ -4,10 +4,43 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
+class Solution1(object):
     def maxDepth(self, root):
         """
         :type root: Optional[TreeNode]
         :rtype: int
         """
-        
+        if root is None:
+            return 0
+
+        left_path = self.maxDepth(root.left)
+        right_path = self.maxDepth(root.right)
+
+        if left_path > right_path:
+            return left_path + 1
+        else:
+            return right_path + 1
+
+
+class Solution2(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if root is None:
+            return 0
+
+        stack = [(root, 1)]
+        max_depth = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                max_depth = max(max_depth, depth)
+                stack.append((node.left, depth + 1))
+                stack.append((node.right, depth + 1))
+
+        return max_depth
+
