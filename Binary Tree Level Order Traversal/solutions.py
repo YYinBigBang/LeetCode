651336d088240ca116533c5e0ef4ed9a04836bc2
YYinBigBang <yy32438084@gmail.com>
2024-12-21 16:05:56 +0800

    Add solution

--- Binary Tree Level Order Traversal/solutions.py
+++ Binary Tree Level Order Traversal/solutions.py
@@ -1,4 +1,7 @@
 # Definition for a binary tree node.
+from collections import deque
+
+
 class TreeNode(object):
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -12,4 +15,25 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[List[int]]
         """
+        if root is None:
+            return []
+
+        queue = deque([(root, 0)])
+        answer = []
+
+        while queue:
+            # Pop the first element from the queue
+            node, depth = queue.popleft()
+            if node:
+                if len(answer) <= depth:
+                    answer.append([])
+
+                # Append the value of the node to the list at the current depth
+                answer[depth].append(node.val)
+
+                # Add the left and right children of the current node to the queue
+                queue.append((node.left, depth + 1))
+                queue.append((node.right, depth + 1))
+
+        return answer
 
